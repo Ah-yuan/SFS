fb2abea849cb215db1661f7ccccf20f62a12b07d
Ah-yuan <73392363+Ah-yuan@users.noreply.github.com>
2023-06-02 11:29:02 +0800

    添加日志注释，理解log

--- SFS-standalone/execute.go
+++ SFS-standalone/execute.go
@@ -72,7 +72,18 @@ func Execute(job PidI, p string, pids chan PidI, core string, queue chan PidI){
         //log4sys.Warn("NumGoroutine:",runtime.NumGoroutine())
 	fmt.Println("logs TIME: ",job.Job, t1.Sub(start_time), t2.Sub(start_time))
 }
-
+```
+这段代码定义了一个名为Execute的函数，它有五个参数：job、p、pids、core和queue。该函数用于执行命令并更新作业方向。
+在函数中，首先根据传入的参数p来决定执行哪个命令。当p等于"N"时，执行的是以下命令：
+schedtool -N -a core -e python fib.py job.N job.Id
+其中，job.N和job.Id是从任务job中获取的参数，用于作为fib.py脚本的输入参数。
+当p不等于"N"时，执行的是以下命令：
+schedtool -F -p 20 -a core -e python fib.py job.N job.Id
+同样，job.N和job.Id也是从任务job中获取的参数。
+执行命令后，会将返回的进程ID存储到pid变量中，并根据它创建一个新的PidI类型的结构体new_pid，并将其加入到队列queue中。接着，等待命令执行完成，并将new_pid发送到pids通道中。
+在控制台输出一个字符串"logs TIME: "，后面跟着任务的ID（job.Job）、任务开始执行的时间（t1.Sub(start_time)）和任务结束执行的时间（t2.Sub(start_time)）。
+其中，t1.Sub(start_time)表示从函数开始执行到任务开始执行的时间间隔，t2.Sub(start_time)表示从函数开始执行到任务结束执行的时间间隔。
+```
 func ExecuteNoChannel(wg *sync.WaitGroup, job Action, p string, pids chan PidI, start_time time.Time, cpuC string){
         defer wg.Done()
         //time.Sleep(time.Duration(job.Start) * time.Millisecond)
