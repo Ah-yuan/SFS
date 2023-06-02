9bc72d40709b17b30be8b0cf054762cedd71c536
Ah-yuan <xunlongwang@outlook.com>
2023-06-02 17:07:47 +0800

    add result dir and make some change

--- SFS-standalone-image/SFS-standalone/test.sh
+++ SFS-standalone-image/SFS-standalone/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./main -p c -t test2 -n 12 > /result/cfs.txt
-./main -p m -t test2 -n 12 > /result/sfs.txt
+./main -p c -t test2 -n 2 > /result/cfs.txt
+./main -p m -t test2 -n 2 > /result/sfs.txt
 #./main -p s -t test1 -n 12 > ./result/srtf.txt
---
+++ result/处理/test1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./main -p c -t test1 -n 2 > /result/cfs.txt
+./main -p m -t test1 -n 2 > /result/sfs.txt
+#./main -p s -t test1 -n 12 > ./result/srtf.txt
